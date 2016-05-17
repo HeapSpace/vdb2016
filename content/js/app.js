764e36c0765b875c5b8c1963225eee07e00694a4
Dejan Tasic <dtasic@gmail.com>
2016-05-17 19:07:33 +0200

    scroll to top update

--- content/js/app.js
+++ content/js/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+    var offset = 500;
+  	var duration = 500;
+  	$(window).scroll(function() {
+    	if ($(this).scrollTop() > offset) {
+    	   $('#scroll-to-top').fadeIn(duration);
+    	} else {
+    	   $('#scroll-to-top').fadeOut(duration);
+    	}
+  	});
+
+  	$('#scroll-to-top').click(function(event) {
+    		event.preventDefault();
+    	$('html, body').animate({scrollTop: 0}, 1200);
+    		return false;
+  	});
+
     $(".float-label").jvFloat();
 
     $('.header-holder').affix({
