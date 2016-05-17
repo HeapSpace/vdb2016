41768f947dc73b835ab2d44a11b0db1d6ccd7fd3
Dejan Tasic <dtasic@gmail.com>
2016-05-17 14:50:34 +0200

    sponsors mail update

--- content/sponsors/sponsor_form.php
+++ content/sponsors/sponsor_form.php
@@ -22,14 +22,29 @@ Phone: $phone <br />
 <br />
 EOD;
 
+$body2 = <<<EOD
+<br />
+Thank you for expressing interest in being a partner of Voxxed Days Belgrade 2016<br />
+Our team will soon reach out to you.<br />
+<br />
+VoxxedDays Belgrade 2016<br />
+<br />
+EOD;
+
 $headers = 'From: ' . $email . "\r\n" .
     'Reply-To: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion() . "\r\n" .
 	'Content-type: text/html' . "\r\n";
 
-$success = mail($emailFrom, $emailSubject, $body, $headers);
+$headers2 = 'From: ' . $emailFrom . "\r\n" .
+    'Reply-To: ' . $emailFrom . "\r\n" .
+    'X-Mailer: PHP/' . phpversion() . "\r\n" .
+	'Content-type: text/html' . "\r\n";
 
-if ($success) {
+$success = mail($emailFrom, $emailSubject, $body, $headers);
+$success2 = mail($email, $emailSubject, $body, $headers);
+
+if ($success && $success2) {
   header("Location: thanks/");
 } else {
   header("Location: error/");
