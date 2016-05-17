82bd535dd8ea01c59c5fe81b2d86ee22ce4d1973
Dejan Tasic <dtasic@gmail.com>
2016-05-17 15:08:09 +0200

    removed test part of URL

--- content/js/app.js
+++ content/js/app.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
 
     $(".float-label").jvFloat();
 
-    $("a").attr("href", function(i, href) {
-      return '/new-site' + href;
-    });
-
     $('.header-holder').affix({
       offset: {
         top: $('header').height() + 1
