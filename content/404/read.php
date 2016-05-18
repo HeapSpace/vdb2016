00e4c848fc6acdf55f3472bb8517eb23b1ea9255
Igor Spasić <igor@jodd.org>
2016-05-18 13:23:07 +0200

    fx

--- content/404/read.php
+++ content/404/read.php
@@ -2,7 +2,7 @@
 
 $filename = "left.txt";
 $fp = fopen($filename , "r") or die ("!?!");
-$count = intval(trim(fgets($fp, 1024)));
+$count = intval(trim(fgets($fp)));
 fclose($fp);
 
 echo "[$count]";
