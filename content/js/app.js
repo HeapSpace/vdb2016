34040ce696c0f4538eea0e79da90811f600644fa
Dejan Tasic <dtasic@gmail.com>
2016-05-19 17:45:22 +0200

    main menu

--- content/js/app.js
+++ content/js/app.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
 
     var offset = 300;
+    var shrinkoffset = 1;
   	var duration = 400;
   	$(window).scroll(function() {
     	if ($(this).scrollTop() > offset) {
     	   $('#scroll-to-top').fadeIn(duration);
     	} else {
     	   $('#scroll-to-top').fadeOut(duration);
+    	}
+      if ($(this).scrollTop() > shrinkoffset) {
+         $('.main-menu-item').addClass('mm-item-pad');
+    	} else {
+         $('.main-menu-item').removeClass('mm-item-pad');
     	}
   	});
 
