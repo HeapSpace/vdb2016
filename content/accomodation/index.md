e4ed3511b0b64ecd7da73799a17cc4ffe66c9065
Dejan Tasic <dtasic@gmail.com>
2016-07-20 09:10:49 +0200

    mera sponsor and accomodation page

---
+++ content/accomodation/index.md
@@ -0,0 +1,4 @@
+---
+title: Accomodation
+layout: '/accomodation.html'
+---
--- lib/map.rb
+++ lib/map.rb
@@ -1,6 +1,6 @@
 include Nanoc::Helpers::LinkTo
 
-def map_on_page(target)
+def map_on_page(target, elid, lat, lon, pin)
   # Find path
   path = target.is_a?(String) ? target : target.path
 
@@ -8,16 +8,16 @@ def map_on_page(target)
     # Create message
 
     "<script>
-    var map;
-    function customMap() {
-      var posLatitude = 44.809871;
-      var posLongitude = 20.413875;
-      var coords = new google.maps.LatLng(posLatitude, posLongitude);
-      var options = {
-        center: coords,
-        home: coords,
-        text: '<div class=\"map-popup\"><h5>Black Glass servis</h5>Novi Beograd<br />Ivana Markovića Irca 17<br />063/ 822-90-96<br />062/ 8-400-400<br />office@blackglass.rs</div>',
-        icon_url: '/images/map-pin-vdb.png',
+    var map"+elid+";
+
+      var posLatitude = "+lat+";
+      var posLongitude = "+lon+";
+      var coords"+elid+" = new google.maps.LatLng(posLatitude, posLongitude);
+      var options"+elid+" = {
+        center: coords"+elid+",
+        home: coords"+elid+",
+        text: '<div class=\"map-popup\">VDB16</div>',
+        icon_url: '/images/"+pin+"',
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControl: false,
@@ -31,35 +31,25 @@ def map_on_page(target)
         scrollwheel: false,
         overviewMapControl: false
       }
-      map = new google.maps.Map(document.getElementById('map'), options);
+      map"+elid+" = new google.maps.Map(document.getElementById('"+elid+"'), options"+elid+");
 
-      var icon = {
-          url: options.icon_url,
+      var icon"+elid+" = {
+          url: options"+elid+".icon_url,
           origin: new google.maps.Point(0, 0)
       };
 
-      var marker = new google.maps.Marker({
-          position: coords,
-          map: map,
-          icon: icon,
+      var marker"+elid+" = new google.maps.Marker({
+          position: coords"+elid+",
+          map: map"+elid+",
+          icon: icon"+elid+",
           draggable: false
       });
 
-      // var info = new google.maps.InfoWindow({
-      //     content: options.text
-      // });
-      //
-      // google.maps.event.addListener(marker, 'click', function() {
-      //     info.open(map, marker);
-      // });
+      var styles"+elid+" = [{'featureType':'all','elementType':'labels.text.fill','stylers':[{'saturation':36},{'color':'#000000'},{'lightness':40}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'visibility':'on'},{'color':'#000000'},{'lightness':16}]},{'featureType':'all','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':17},{'weight':1.2}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':21}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':17}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':29},{'weight':0.2}]},{'featureType':'road.arterial','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':18}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':16}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':19}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#2e230f'},{'lightness':17}]}];
 
+      map"+elid+".setOptions({styles: styles"+elid+"});
 
-      var styles = [{'featureType':'all','elementType':'labels.text.fill','stylers':[{'saturation':36},{'color':'#000000'},{'lightness':40}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'visibility':'on'},{'color':'#000000'},{'lightness':16}]},{'featureType':'all','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':17},{'weight':1.2}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':21}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':17}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':29},{'weight':0.2}]},{'featureType':'road.arterial','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':18}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':16}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':19}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#2e230f'},{'lightness':17}]}];
-
-      map.setOptions({styles: styles});
-    }
-    </script>
-    <script src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyA3JQOqnA29_8q982bNgzDPPX3gsVqK5oY&callback=customMap\"></script>"
+    </script>"
 
   end
 end
