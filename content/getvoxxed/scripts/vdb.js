b0a8a711e66e0cf0df06f834dc4054bc5340936e
Igor Spasić <igor@jodd.org>
2016-08-07 20:12:37 +0200

    facebook tryout

--- content/getvoxxed/scripts/vdb.js
+++ content/getvoxxed/scripts/vdb.js
@@ -301,3 +301,105 @@ if (window.location.hostname != "localhost") {
 	    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 	}
 }
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1808950409337325',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+
+function postImageToFacebook(token, filename, mimeType, imageData, message) {
+    var fd = new FormData();
+    fd.append("access_token", token);
+    fd.append("source", imageData);
+    fd.append("no_story", true);
+
+    // Upload image to facebook without story(post to feed)
+    $.ajax({
+        url: "https://graph.facebook.com/me/photos?access_token=" + token,
+        type: "POST",
+        data: fd,
+        processData: false,
+        contentType: false,
+        cache: false,
+        success: function (data) {
+            console.log("success: ", data);
+
+            // Get image source url
+            FB.api(
+                "/" + data.id + "?fields=images",
+                function (response) {
+                    if (response && !response.error) {
+                        //console.log(response.images[0].source);
+
+                        // Create facebook post using image
+                        FB.api(
+                            "/me/feed",
+                            "POST",
+                            {
+                                "message": "",
+                                "picture": response.images[0].source,
+                                "link": window.location.href,
+                                "name": 'Look at the cute panda!',
+                                "description": message,
+                                "privacy": {
+                                    value: 'SELF'
+                                }
+                            },
+                            function (response) {
+                                if (response && !response.error) {
+                                    /* handle the result */
+                                    console.log("Posted story to facebook");
+                                    console.log(response);
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+        },
+        error: function (shr, status, data) {
+            console.log("error " + data + " Status " + shr.status);
+        },
+        complete: function (data) {
+            //console.log('Post to facebook Complete');
+        }
+    });
+}
+
+$('#shareFB').click(function () {
+	var snap = document.getElementById('flatten');
+    var data = snap.toDataURL("image/png");
+    try {
+        blob = dataURItoBlob(data);
+    } catch (e) {
+        console.log(e);
+    }
+    FB.getLoginStatus(function (response) {
+        console.log(response);
+        if (response.status === "connected") {
+            postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
+        } else if (response.status === "not_authorized") {
+            FB.login(function (response) {
+                postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
+            }, {scope: "publish_actions"});
+        } else {
+            FB.login(function (response) {
+                postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
+            }, {scope: "publish_actions"});
+        }
+    });
+});
