283aaaf6c48b25b5dcdce73d8ec29ddf3eba69f5
Dejan Tasic <dtasic@gmail.com>
2016-10-04 03:02:25 +0200

    twitter slider

--- content/js/app.js
+++ content/js/app.js
@@ -250,45 +250,20 @@ $(document).ready(function() {
       }
     });
 
-
-    // $("#flatmenu").menumaker({
-    //     title: "Menu",
-    //     breakpoint: 768,
-    //     format: "multitoggle"
-    // });
-    //
-    // $("#flatmenu").prepend("<div id='menu-line'></div>");
-    //
-    // var foundActive = false, activeElement, linePosition = 0, menuLine = $("#flatmenu #menu-line"), lineWidth, defaultPosition, defaultWidth;
-    //
-    // $("#flatmenu > ul > li").each(function() {
-    //     if ($(this).hasClass('active')) {
-    //         activeElement = $(this);
-    //         foundActive = true;
-    //     }
-    // });
-    //
-    // if (foundActive === false) {
-    //     activeElement = $("#flatmenu > ul > li").first();
-    // }
-    //
-    // defaultWidth = lineWidth = activeElement.width();
-    //
-    // defaultPosition = linePosition = activeElement.position().left;
-    //
-    // menuLine.css("width", lineWidth);
-    // menuLine.css("left", linePosition);
-    //
-    // $("#flatmenu > ul > li").hover(function() {
-    //     activeElement = $(this);
-    //     lineWidth = activeElement.width();
-    //     linePosition = activeElement.position().left;
-    //     menuLine.css("width", lineWidth);
-    //     menuLine.css("left", linePosition);
-    // }, function() {
-    //     menuLine.css("left", defaultPosition);
-    //     menuLine.css("width", defaultWidth);
-    // });
+    $('#tweetslider').slick({
+      initialSlide: 3,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 7500,
+      nextArrow: '<button type="button" class="btn btn-primary btn-lg blue">Next</button>',
+      prevArrow: '<button type="button" class="btn btn-primary btn-lg blue">Previous</button>',
+      appendArrows: $('#slidernav'),
+      adaptiveHeight: true,
+      infinite: true,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      respondTo: 'slider'
+    });
 
     $('.navbar-toggle').on('click', function(){
         $('#navmenu').css('display', 'block');
